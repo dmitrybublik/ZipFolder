c3aba93d827ef149d5f6c801f072c8dbfebb9294
dmitry.bublik <dmitry.buba@gmail.com>
2016-03-06 11:19:39 +0200

    Initial commit

---
+++ ZipFolder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Compression;
+
+namespace ZipFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var folderName = args[0];
+            var archiveName = args[1];
+            ZipFile.CreateFromDirectory(folderName, archiveName, CompressionLevel.Optimal, true);
+        }
+    }
+}
